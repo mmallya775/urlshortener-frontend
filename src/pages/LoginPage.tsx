86fe73eaac7b31027c2c43a80622803dd6b80f66
Mohit Radhesh Mallya <mmallya775@gmail.com>
2026-09-09 12:37:57 +0200

    refactor: update asset files and improve LoginPage UI
    - Remove unused `icons.svg`.
    - Update favicon and logo assets with project specific icons.
    - Modify `LoginPage` to use new logo and adjust styles.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,3 @@
-import {GalleryVerticalEnd} from "lucide-react"
-
 import {LoginForm} from "@/components/login-form"
 import {useAuth} from "@/auth/AuthContext"
 import {useNavigate} from "react-router";
@@ -43,8 +41,8 @@ export default function LoginPage() {
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="/" className="flex items-center gap-2 font-medium">
-            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4"/>
+            <div className="flex size-8 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <img src={"/favicon.svg"} className={"scale-75"} alt={"Smallify Icon"}/>
             </div>
             SmallifyURL
           </a>
@@ -58,7 +56,7 @@ export default function LoginPage() {
         </div>
       </div>
       <div className="relative hidden bg-muted lg:block">
-        <img src="/icons.svg" alt="Image"
+        <img src="/background.png" alt="Smallify Background Image"
              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"/>
       </div>
     </div>
