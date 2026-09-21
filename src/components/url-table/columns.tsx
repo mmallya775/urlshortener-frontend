759d3a63173f46b9f33f64cda72feee742990e33
Mohit Radhesh Mallya <mmallya775@gmail.com>
2026-09-21 18:35:41 +0530

    add: changed the rendering of created at date to show custom formatted date

--- src/components/url-table/columns.tsx
+++ src/components/url-table/columns.tsx
@@ -5,7 +5,23 @@ import type {Urls} from "@/types/urlTypes.ts";
 const columnHelper = createColumnHelper<DataTableFeatures, Urls>()
 
 export const columns = columnHelper.columns([
-  columnHelper.accessor("createdAt", {header: "Created At"}),
+  columnHelper.accessor("createdAt", {
+    header: "Created At", cell: (info) => {
+      const date = new Date(info.getValue());
+
+      const formattedDate = new Intl.DateTimeFormat("de-DE", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+        timeZoneName: "short",
+      }).format(date);
+
+      return <span>{formattedDate}</span>;
+    }
+  }),
   columnHelper.accessor("mainUrl", {
     header: "Main Url", cell: (info) => {
       const mainUrl = info.getValue().trim()
@@ -14,7 +30,8 @@ export const columns = columnHelper.columns([
         ? mainUrl
         : `https://${mainUrl}`
 
-      return <a href={href} target="_blank" rel="noopener noreferrer" className={"text-blue-700 dark:text-blue-400 hover:underline"}>
+      return <a href={href} target="_blank" rel="noopener noreferrer"
+                className={"text-blue-700 dark:text-blue-400 hover:underline"}>
         {mainUrl}</a>
     }
   }),
